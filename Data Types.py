56f5293e0e9a330d67b4e4e1cfc11dec6ff9e43b
ghanashyambudhathoki01 <sdev1252492@gmail.com>
2025-05-16 17:42:03 +0545

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,36 @@
+#chapter  data types 
+#Airthmetic Operators 
+a = 7 
+b = 2
+c = (a+b)
+print (c)
+
+#Airthmetic Operators are : +,-,/,*, 
+
+#Assignment Operators 
+d = 4-2 
+e = 6 
+e +=2
+print(e)
+#Assignment Operators : =,+=,-= etc
+
+#Comparison Operators
+f = 5<4 
+print (f)
+#Comparison Operators are : ==,>,>=,<,!,= etc 
+
+#Logical Operators 
+e = True or False
+print (e)
+g = False and False
+print (g)
+
+# True or false is True 
+# True or True is True 
+# False or True is True 
+# False or False is False 
+
+# True and false is False
+# True and True is True 
+# False and True is True 
+# False and False is False
---
+++ varibales.py
@@ -0,0 +1,9 @@
+#chapter one Variables and Datatypes
+
+a = 12 # this is an a integer 
+b = 12.32 # this is a floating 
+c = "shyam" # this is a string
+d = False # this is a boolean value 
+e = None # this is a none type variable 
+print (a+b)
+print (d)
